8845046d5e0e2a6c7bae94418af6132f936bfa97
shtunder <shtunder.andrey1@yandex.by>
2020-02-05 23:39:27 +0300

    Added Toolbar

--- app/src/main/java/com/example/tedesk/MenuActivity.java
+++ app/src/main/java/com/example/tedesk/MenuActivity.java
@@ -2,13 +2,35 @@ package com.example.tedesk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        initToolbar();
+    }
+
+    @TargetApi(21)
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+
     }
 }
