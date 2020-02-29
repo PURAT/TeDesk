100684e2ce695177aac99b0abab7a003342da6a3
SHTUNDER <shtunder.andrey1yandex.by>
2020-03-01 00:40:38 +0300

    Improved NavigationView

---
+++ app/src/main/java/com/example/tedesk/models/User.java
@@ -0,0 +1,46 @@
+package com.example.tedesk.models;
+
+public class User {
+    private String email, password, name, surname;
+
+    public User() {}
+
+    public User(String email, String password, String name, String surname) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
